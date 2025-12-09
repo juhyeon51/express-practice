6a1300952254644257ddf906026d2e009b767382
Juhyeon Park <juhyeon51@ajou.ac.kr>
2025-12-09 16:22:37 +0900

    Edit port in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const firebase = require('./firebase');
 const cors = require('cors');
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}));
